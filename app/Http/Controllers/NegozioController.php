934f7ab69d55c988445e7bdacb561f79596e324a
Davis Cacici <Antoninodaviscacici@gmail.com>
2021-04-22 16:13:19 +0200

    .

--- app/Http/Controllers/NegozioController.php
+++ app/Http/Controllers/NegozioController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\articolo;
-use App\Models\magazzino;
 use App\Models\tipologia;
 use App\Models\marca;
 use App\Models\negozio;
